bde3191b963b15dd27d15d83c0c32381bc2a4fe6
Pavel Gonzales <develop.gonzales@gmail.com>
2017-04-07 16:54:05 +0300

    потюнил диалоговое окно

--- src/components/info-dialog.vue
+++ src/components/info-dialog.vue
@@ -1,7 +1,13 @@
 <template lang="pug">
-  .dialog
-    h1 Dialog
-    button(@click="$emit('close')") close
+  transition(name="fade")
+    .dialog
+      .overlay(@click="$emit('close')")
+      .inner
+        h1 Название мероприятия
+        h3 время и место
+        p Описание мероприятия
+        .map карта
+        button(@click="$emit('close')") close
 </template>
 
 <script>
@@ -19,5 +25,40 @@ export default {
   left 0
   right 0
   bottom 0
-  background-color rgba(0,0,0,0.4)
+  display flex 
+  justify-content center 
+  align-items center
+.overlay
+  position absolute
+  top 0
+  left 0
+  right 0
+  bottom 0
+  background-color rgba(0,0,0,0.7)
+
+.inner
+  position relative
+  background-color #fff
+  border-radius 3px
+  padding 24px
+  transition transform .4s ease-in-out
+
+.map 
+  width 540px
+  height 320px
+  background-color #efefef
+
+button 
+  margin-top 15px
+
+.fade-enter-active
+.fade-leave-active
+  transition opacity .5s
+
+.fade-enter
+.fade-leave-to /* .fade-leave-active для <2.1.8 */
+  opacity 0
+  .inner 
+    transform scale(1.2)
+
 </style>
